66604a045cae0af51a04f6f35ea1f9ce3f49714c
Paul <hughpaud@gmail.com>
2021-02-25 13:18:10 -0600

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/* 
+    This is a simple Java App.
+    Call this file "App.java".
+*/
+
+
+
+
+public class App {
+    // Your program begins with a call to main()
+    public static void main(String[] args) throws Exception {
+        System.out.println("This is a simple Java program!");
+    }
+}
